03bec9d2dcc6c766c10acde5bf7f87cf89715f06
hlima-2021464 <hlima-2021464@kinal.edu.gt>
2026-03-27 23:48:27 -0600

    feat: se creo un repositorio para DetalleVenta

---
+++ src/main/java/com/henrylima/kinalapp/repository/DetalleVentaRepository.java
@@ -0,0 +1,7 @@
+package com.henrylima.kinalapp.repository;
+
+import com.henrylima.kinalapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+}
