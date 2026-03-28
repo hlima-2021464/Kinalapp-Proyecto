8941c78b1861097e3a052b44ef78b9ff54a4712d
hlima-2021464 <hlima-2021464@kinal.edu.gt>
2026-03-27 23:47:02 -0600

    feat: se creo la entidad DetalleVenta

---
+++ src/main/java/com/henrylima/kinalapp/model/DetalleVenta.java
@@ -0,0 +1,67 @@
+package com.henrylima.kinalapp.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "detalle_ventas")
+public class DetalleVenta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDetalleVenta;
+
+    private Long cantidad;
+
+    private BigDecimal precio;
+
+    @ManyToOne
+    @JoinColumn(name = "idVenta")
+    private Venta venta;
+
+    @ManyToOne
+    @JoinColumn(name = "idProducto")
+    private Producto producto;
+
+    public DetalleVenta() {}
+
+    public Long getIdDetalleVenta() {
+        return idDetalleVenta;
+    }
+
+    public void setIdDetalleVenta(Long idDetalleVenta) {
+        this.idDetalleVenta = idDetalleVenta;
+    }
+
+    public Long getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Long cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
