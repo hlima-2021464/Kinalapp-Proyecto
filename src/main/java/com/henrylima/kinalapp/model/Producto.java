08f66e5350274c3e695341c5282fd80c201d2e78
hlima-2021464 <hlima-2021464@kinal.edu.gt>
2026-03-27 23:34:33 -0600

    feat: crear entidad Producto

---
+++ src/main/java/com/henrylima/kinalapp/model/Producto.java
@@ -0,0 +1,74 @@
+package com.henrylima.kinalapp.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_producto")
+    private Long codigoProducto;
+
+    @Column(name = "nombre_producto", length = 60)
+    private String nombreProducto;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal precio;
+
+    private Long stock;
+
+    private Long estado;
+
+    public Producto() {
+    }
+
+    public Producto(String nombreProducto, BigDecimal precio, Long stock, Long estado) {
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public Long getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(Long codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Long getStock() {
+        return stock;
+    }
+
+    public void setStock(Long stock) {
+        this.stock = stock;
+    }
+
+    public Long getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Long estado) {
+        this.estado = estado;
+    }
+}
