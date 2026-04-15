8d54480647ceaff40bfd86bab99adde79bef1972
hlima-2021464 <hlima-2021464@kinal.edu.gt>
2026-04-15 09:07:53 -0600

    fix: se arreglo una línea de código en VentaService.java

--- src/main/java/com/henrylima/kinalapp/service/VentaService.java
+++ src/main/java/com/henrylima/kinalapp/service/VentaService.java
@@ -2,7 +2,7 @@ package com.henrylima.kinalapp.service;
 
 import com.henrylima.kinalapp.entity.Venta;
 import com.henrylima.kinalapp.repository.VentaRepository;
-import org.springframework.stereotype.service;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
