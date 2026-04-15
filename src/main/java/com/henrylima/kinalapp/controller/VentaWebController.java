9c1dbf786e86171c29e0838fb003ff0431da4f13
hlima-2021464 <hlima-2021464@kinal.edu.gt>
2026-04-15 08:58:20 -0600

    fix: se arreglo una línea de código en VentaWebController.java

--- src/main/java/com/henrylima/kinalapp/controller/VentaWebController.java
+++ src/main/java/com/henrylima/kinalapp/controller/VentaWebController.java
@@ -1,6 +1,6 @@
 package com.henrylima.kinalapp.controller;
 
-import com.henrylima.kinalapp.Service.IVentaService;
+import com.henrylima.kinalapp.service.IVentaService;
 import com.henrylima.kinalapp.entity.Usuario;
 import com.henrylima.kinalapp.entity.Venta;
 import jakarta.servlet.http.HttpSession;
