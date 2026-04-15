4fe6d38bc5bf892ad432461e1a7a9f3bfe96d01d
hlima-2021464 <hlima-2021464@kinal.edu.gt>
2026-04-15 08:57:34 -0600

    fix: se arreglo una línea de código en UsuarioWebController.java

--- src/main/java/com/henrylima/kinalapp/controller/UsuarioWebController.java
+++ src/main/java/com/henrylima/kinalapp/controller/UsuarioWebController.java
@@ -1,6 +1,6 @@
 package com.henrylima.kinalapp.controller;
 
-import com.henrylima.kinalapp.Service.IUsuarioService;
+import com.henrylima.kinalapp.service.IUsuarioService;
 import com.henrylima.kinalapp.entity.Usuario;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
