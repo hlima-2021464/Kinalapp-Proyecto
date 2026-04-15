a14444e34277c96af8073c577e79914009c55a27
hlima-2021464 <hlima-2021464@kinal.edu.gt>
2026-04-15 08:56:28 -0600

    fix: se arreglo una línea de código en ProductoWebController

--- src/main/java/com/henrylima/kinalapp/controller/ProductoWebController.java
+++ src/main/java/com/henrylima/kinalapp/controller/ProductoWebController.java
@@ -1,6 +1,6 @@
 package com.henrylima.kinalapp.controller;
 
-import com.henrylima.kinalapp.Service.IProductoService;
+import com.henrylima.kinalapp.service.IProductoService;
 import com.henrylima.kinalapp.entity.Producto;
 import com.henrylima.kinalapp.entity.Usuario;
 import jakarta.servlet.http.HttpSession;
